0670d8254d120707a5e57873c798b6a9e586e2f4
DepressedCookie <vipc.1224@live.ca>
2020-09-27 20:35:40 -0400

    sigh

--- Assets/NetworkMan.cs
+++ Assets/NetworkMan.cs
@@ -5,13 +5,29 @@ using System;
 using System.Text;
 using System.Net.Sockets;
 using System.Net;
+using Unity.Mathematics;
+using System.Security.Cryptography;
+
 
 public class NetworkMan : MonoBehaviour
 {
+    List<GameObject> AllPlayers = new List<GameObject>();
+    Player ThisPlayer = new Player();
+    int NPlayersCount = 0;
+
+    // The player prefab
+    [SerializeField]
+    GameObject PlayerPrefab;
+ 
+    // All the spawn points in the world
+    GameObject[] SpawnPoints;
+
     public UdpClient udp;
     // Start is called before the first frame update
     void Start()
     {
+        SetupSpawnPoints();
+
         udp = new UdpClient();
 
         bool useLocalhost = true;
@@ -31,6 +47,11 @@ public class NetworkMan : MonoBehaviour
         InvokeRepeating("HeartBeat", 1, 1);
     }
 
+    void SetupSpawnPoints()
+    {
+        SpawnPoints = GameObject.FindGameObjectsWithTag("Spawnpoint");
+    }
+
     void OnDestroy(){
         udp.Dispose();
     }
@@ -38,7 +59,9 @@ public class NetworkMan : MonoBehaviour
 
     public enum commands{
         NEW_CLIENT,
-        UPDATE
+        UPDATE,
+        DISCONNECT,
+        OLD_CLIENT
     };
     
     [Serializable]
@@ -55,13 +78,14 @@ public class NetworkMan : MonoBehaviour
             public float B;
         }
         public string id;
-        public receivedColor color;        
+        public receivedColor color;
     }
 
     [Serializable]
     public class NewPlayer{
-        
+        public Player player;
     }
+
 
     [Serializable]
     public class GameState{
@@ -69,7 +93,10 @@ public class NetworkMan : MonoBehaviour
     }
 
     public Message latestMessage;
+    public NewPlayer NPlayer;
     public GameState lastestGameState;
+    public GameState connectedPlayers;
+    public Player DCPlayer;
     void OnReceived(IAsyncResult result){
         // this is what had been passed into BeginReceive as the second parameter:
         UdpClient socket = result.AsyncState as UdpClient;
@@ -88,9 +115,17 @@ public class NetworkMan : MonoBehaviour
         try{
             switch(latestMessage.cmd){
                 case commands.NEW_CLIENT:
+                    NPlayer = JsonUtility.FromJson<NewPlayer>(returnData);
+                    NPlayersCount++;
                     break;
                 case commands.UPDATE:
                     lastestGameState = JsonUtility.FromJson<GameState>(returnData);
+                    break;
+                case commands.DISCONNECT:
+                    DCPlayer = JsonUtility.FromJson<Player>(returnData);
+                    break;
+                case commands.OLD_CLIENT:
+                    connectedPlayers = JsonUtility.FromJson<GameState>(returnData);
                     break;
                 default:
                     Debug.Log("Error");
@@ -105,12 +140,78 @@ public class NetworkMan : MonoBehaviour
         socket.BeginReceive(new AsyncCallback(OnReceived), socket);
     }
 
-    void SpawnPlayers(){
+
+    void SpawnPlayers()
+    {
+
+        if (NPlayersCount > 0)
+        {
+            int RandomPoint = UnityEngine.Random.Range(0, SpawnPoints.Length);
+            GameObject playerObj = Instantiate(PlayerPrefab, SpawnPoints[RandomPoint].transform.position, Quaternion.identity);
+            playerObj.GetComponent<NetInfo>().ID = NPlayer.player.id;
+            AllPlayers.Add(playerObj);
+            NPlayersCount--;
+        }
+        else
+        {
+            if (connectedPlayers.players.Length > AllPlayers.Count)
+            {
+                foreach (Player p in connectedPlayers.players)
+                {
+                    int RandomPoint = UnityEngine.Random.Range(0, SpawnPoints.Length);
+                    GameObject playerObj = Instantiate(PlayerPrefab, SpawnPoints[RandomPoint].transform.position, Quaternion.identity);
+                    playerObj.GetComponent<NetInfo>().ID = p.id;
+                    AllPlayers.Add(playerObj);
+                }
+            }
+        }
+
+        //if (lastestGameState.players.Length > AllPlayers.Count)
+        //{
+        //    foreach (Player p in lastestGameState.players)
+        //    {
+        //        if (p.id != ThisPlayer.id)
+        //        {
+        //            int RandomPoint = UnityEngine.Random.Range(0, SpawnPoints.Length);
+        //            GameObject playerObj = Instantiate(PlayerPrefab, SpawnPoints[RandomPoint].transform.position, Quaternion.identity);
+        //            playerObj.GetComponent<NetInfo>().ID = p.id;
+        //            AllPlayers.Add(playerObj);
+        //            ThisPlayer = p;
+        //        }
+        //    }
+        //}
+        //else
+        //{
+        //    if (NPlayer != null)
+        //    {
+        //        Player p = NPlayer.player;
+        //        if (p.id.Length > 0 && ThisPlayer != p)
+        //        {
+        //            Debug.Log("Net: " + p.id + " New: " + NPlayer.player.id);
+        //            int RandomPoint = UnityEngine.Random.Range(0, SpawnPoints.Length);
+        //            GameObject playerObj = Instantiate(PlayerPrefab, SpawnPoints[RandomPoint].transform.position, Quaternion.identity);
+        //            playerObj.GetComponent<NetInfo>().ID = p.id;
+        //            AllPlayers.Add(playerObj);
+        //            ThisPlayer = p;
+        //        }
+        //    }
+        //}
 
     }
 
-    void UpdatePlayers(){
-
+    void UpdatePlayers()
+    {
+        foreach (Player p in lastestGameState.players)
+        {
+            foreach (GameObject PO in AllPlayers)
+            {
+                //Debug.Log(p.id + " and " + PO.GetComponent<NetInfo>().ID);
+                if (p.id.Equals(PO.GetComponent<NetInfo>().ID))
+                {
+                    PO.GetComponent<NetInfo>().UpdateMat(new Color(p.color.R, p.color.G, p.color.B));
+                }
+            }
+        }
     }
 
     void DestroyPlayers(){
